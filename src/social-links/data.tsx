64e746f197e0f2c35948b6ca456e37c1b34255c6
Ghost9841 <asubedi778@gmail.com>
2025-06-17 17:01:13 +0545

    Links are updated

--- src/social-links/data.tsx
+++ src/social-links/data.tsx
@@ -1,36 +1,38 @@
-import { FaDiscord } from "react-icons/fa"
-import { Instagram, Facebook, Twitter, Youtube, MessageCircle, Send } from "lucide-react";
+import { FaDiscord, } from "react-icons/fa"
+import {  Send } from "lucide-react";
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
 
 
 export const socialLinks = [
     {
         name: "Instagram",
-        icon: <Instagram className="w-5 h-5" />,
-      url: "https://www.instagram.com/_tech.nest_/",
+        icon: <BsInstagram className="w-5 h-5" />,
+      url: "https://www.instagram.com/_tech.nest__/",
         color: "bg-gradient-to-r from-purple-500 to-pink-500",
     },
     {
         name: "Facebook",
-        icon: <Facebook className="w-5 h-5" />,
+        icon: <BsFacebook className="w-5 h-5" />,
       url: "https://www.facebook.com/people/Tech-Nest/61573879135057/",
         color: "bg-blue-600",
     },
     {
         name: "Twitter",
-        icon: <Twitter className="w-5 h-5" />,
+        icon: <FaXTwitter className="w-5 h-5" />,
       url: "http://x.com/TechNest1159276",
         color: "bg-black",
     },
     {
         name: "YouTube",
-        icon: <Youtube className="w-5 h-5" />,
-      url: "#",
+        icon: <BsYoutube className="w-5 h-5" />,
+      url: "https://www.youtube.com/@technest-0",
         color: "bg-red-600",
     },
     {
         name: "Discord",
         icon: <FaDiscord className="w-5 h-5" />,
-      url: "#",
+      url: "https://discord.gg/su4PeRgjeq",
         color: "bg-indigo-600",
     },
     {
@@ -45,31 +47,31 @@ export const mainLinks = [
     {
         title: "Tech Nest Discord Server",
         icon: <FaDiscord className="w-8 h-8 text-indigo-600" />,
-      url: "#",
-        description: "Join our community discussions",
+      url: "https://discord.gg/su4PeRgjeq",
+        description: "Join our community for free IT course",
     },
     {
         title: "Tech Nest Telegram",
         icon: <Send className="w-8 h-8 text-blue-500" />,
-      url: "#",
+      url: "https://t.me/+PVAyjZ9egO4xODJl",
         description: "Get instant updates and news",
     },
     {
         title: "YouTube",
-        icon: <Youtube className="w-8 h-8 text-red-600" />,
-      url: "#",
+        icon: <BsYoutube className="w-8 h-8 text-red-600" />,
+      url: "https://www.youtube.com/@technest-0",
         description: "Watch our latest tech content",
     },
     {
         title: "Facebook",
-        icon: <Facebook className="w-8 h-8 text-blue-600" />,
-      url: "#",
+        icon: <BsFacebook className="w-8 h-8 text-blue-600" />,
+      url: "https://www.facebook.com/people/Tech-Nest/61573879135057/",
         description: "Follow us for daily updates",
     },
     {
         title: "Instagram",
-        icon: <Instagram className="w-8 h-8 text-pink-600" />,
-      url: "#",
+        icon: <BsInstagram className="w-8 h-8 text-pink-600" />,
+      url: "https://www.instagram.com/_tech.nest__/",
         description: "Behind the scenes content",
     },
 ]
