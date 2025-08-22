41a0a72696b5dd00ecdbacf8dd3012c93fbaa42d
Ghost9841 <asubedi778@gmail.com>
2025-08-22 17:02:06 +0545

    aaba garnu parxa yo ni

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import SocialLinks from "./social-links/sociallinks"
 function App() {
   return (
     <>
+     <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-300 py-8 px-4">
+
       <SocialLinks />
+     </div>
     </>
   )
 }
