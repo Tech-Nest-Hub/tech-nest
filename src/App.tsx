ebda96b52f0176b92809e58019851a03cdd8db74
Ghost9841 <asubedi778@gmail.com>
2025-11-10 19:19:23 +0545

    added sanity

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SocialLinks from "./social-links/sociallinks"
 function App() {
   return (
     <>
-     <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-300 py-8 px-4">
+     <div className="min-h-screen bg-gradient-to-br from-gray-200 to-gray-300">
       <Navbar/>
       <SocialLinks />
       <Footer/>
