d1983398914607e03dedb77323ed23227eba9c9f
Ghost9841 <asubedi778@gmail.com>
2025-11-10 20:59:58 +0545

    Dayum kati choti garnu parney ho

---
+++ tech-nest-sanity/sanity.cli.ts
@@ -0,0 +1,12 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fyydph8c',
+    dataset: 'production'
+  },
+  deployment: {
+   appId: 'zxz0b00gxvcf9o2ct78junxo',
+    autoUpdates: true,
+  }
+})
